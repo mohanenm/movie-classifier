60eff0db59ead0f884c4782a348de5a0b093f2c4
mohanenm <ethannmohan@gmail.com>
2018-03-23 08:51:43 -0500

    scikit

--- split_tvt.py
+++ split_tvt.py
@@ -1,6 +1,8 @@
 import
 
  ''' convert text into vectors'''
+ ''
+ ''
 
  classifier = LinearSVC(c = 0.1) ''' c = 0.10, c = 0.100: one of these will be the most accurate!
  find through validation set: for hw'''
