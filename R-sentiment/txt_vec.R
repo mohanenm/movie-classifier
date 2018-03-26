c19dc80d71dbbed9c4e817bf629925fa521d137c
mohanenm <ethannmohan@gmail.com>
2018-03-26 09:18:40 -0500

    added comments

--- R-sentiment/txt_vec.R
+++ R-sentiment/txt_vec.R
@@ -2,3 +2,12 @@
 text <- ("file for changing text to vector for train, validation, test")
 print(noquote(text))
 
+
+if(FALSE) {
+
+    doesnt R, store text as vectors, so scikit the data is already how it should be?
+    or is this wrong
+}
+
+# scikit learn library for python?
+
